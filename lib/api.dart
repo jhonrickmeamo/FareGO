d30e09e5ff013a837c7e4ee30e1352ac375cbacc
jhonrickmeamo <jhonrickmeamo05@gmail.com>
2025-09-27 00:13:05 +0800

    committt

--- lib/api.dart
+++ lib/api.dart
@@ -1,5 +1,5 @@
 const String googleAPIKey = "AIzaSyAweCEzaFrEE8s-rDWdZzhl33Gl7VsEk8o";
-const String paymongoPkTestKey = "pk_test_DdpLxAAfunvq5nJowRoEEd7s";
-const String payMongoSkTestKey = "sk_test_Z8dcEgmYzGBdSzdMVaymmM3C";
-const String paymongoPublicLiveKey = "pk_live_sofxKsGvNGWXesBibK8wsWXU";
-const String payMongoSecretLiveKey = "sk_live_Fkgia4rf9acXnmahosYUfcj2";
+const String stripeLivePKey = "STRIPE_PUBLIC_KEY";
+const String stripeLiveSKey = "STRIPE_SECRET_KEY";
+const String stripeTestPKey = "STRIPE_TEST_PUBLIC_KEY";
+const String stripeTestSKey = "STRIPE_TEST_SECRET_KEY";
