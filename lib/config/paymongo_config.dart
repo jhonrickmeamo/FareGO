f294e7a335b5e59d489773e739b3e66f7c566f7b
jhonrickmeamo <jhonrickmeamo05@gmail.com>
2025-09-26 23:58:11 +0800

    pang 6th na to

--- lib/config/paymongo_config.dart
+++ lib/config/paymongo_config.dart
@@ -3,11 +3,9 @@ import 'package:farego/api.dart';
 class payMongoConfig {
   static const bool useLive = false;
 
-  static String get publicKey =>
-      useLive ? paymongoPublicLiveKey : paymongoPkTestKey;
+  static String get publicKey => useLive ? stripeLivePKey : stripeTestPKey;
 
-  static String get secretKey =>
-      useLive ? payMongoSecretLiveKey : payMongoSkTestKey;
+  static String get secretKey => useLive ? stripeLiveSKey : stripeTestSKey;
 
   static String get baseUrl => "https://api.paymongo.com/v1";
 }
