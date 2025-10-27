356e3fb514a53a9bdf9ccdbc1b722fbd3dd4d5d3
CARLO BATISTA <bautista.294631@globalcity.sti.edu.ph>
2025-10-27 21:16:44 +0800

    fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,17 +1,8 @@
-import java.util.Properties
-
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-}
-
-val localProperties = Properties()
-val localPropertiesFile = rootProject.file("local.properties")
-if (localPropertiesFile.exists()) {
-    localPropertiesFile.reader(Charsets.UTF_8).use { reader ->
-        localProperties.load(reader)
-    }
+    id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
@@ -33,19 +24,21 @@ android {
         applicationId = "com.example.farego"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 23
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-
-        // ✅ Added for large projects to avoid 65K method limit
-        multiDexEnabled = true
     }
 
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+flutter {
+    source = "../.."
 }
